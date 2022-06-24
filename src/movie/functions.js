9655f6c1674200ad6b34a9d815e5e940dcec9878
DomGibson <103586347+DomGibson@users.noreply.github.com>
2022-06-24 11:29:00 +0100

    Update functions.js

--- src/movie/functions.js
+++ src/movie/functions.js
@@ -7,5 +7,40 @@ exports.addMovie = async (movieObj) => {
     } catch (error) {
         console.log(error);
     }
-    
-}
+};
+
+exports.listMovie = async () => {
+    try {
+        const response = await Movie.find();
+        console.log(response)
+    } catch {
+        console.log(error)
+    };
+};
+
+exports.updateMovie = async (movieObj) => {
+    try {
+        const response = await Movie.findOneAndUpdate({
+                title: movieObj.title
+            }, {
+                $set: {
+                    title: movieObj.newTitle,
+                }
+            }, {
+                new: true
+            }
+        );
+        console.log(response.modifiedCount > 0);
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+exports.delMovie = async (movieObj) => {
+    try {
+        const response = await Movie.deleteOne(movieObj);
+        console.log(response.deletedCount > 0);
+    } catch (error) {
+        console.log(error)
+    }
+};
