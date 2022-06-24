0ddfcff442227b9d576461d383f35f9da70c2535
DomGibson <103586347+DomGibson@users.noreply.github.com>
2022-06-24 11:25:22 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,33 @@
 require("./db/connection");
 const yargs = require("yargs");
 const mongoose = require("mongoose");
-const {addMovie} = require("./movie/functions");
+const {addMovie, listMovie, updateMovie, delMovie} = require("./movie/functions");
 
 const app = async (yargsObj) => {
     if (yargsObj.add) {
         //add movie to database from yargs input
-        await addMovie({title: yargsObj.title, actor: yargsObj.actor})
+        await addMovie({
+            title: yargsObj.title,
+            actor: yargsObj.actor
+        });
     } else if (yargsObj.list) {
         //find movies
+        await listMovie();
     } else if (yargsObj.update) {
         //updates movies
+        await updateMovie({
+            title: yargsObj.title,
+            newTitle: yargsObj.newTitle
+        });
     } else if (yargsObj.delete) {
         //deletes movies
+        await delMovie({
+            title: yargsObj.title
+        });
     } else {
         console.log("Incorrect Command");
     }
+    await mongoose.disconnect();
 }
 
 app(yargs.argv)
